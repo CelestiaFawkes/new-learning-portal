9c4d6e75b13ba10a84d7c80f9577c7e208788772
Rajeshwari Kapse <kapserajeshwari8@gmail.com>
2024-02-09 13:14:01 +0530

    Delete learning-portal directory

--- learning-portal/src/main/java/com/raji/learningportalnew/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.raji.learningportalnew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.raji.learningportalnew.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
